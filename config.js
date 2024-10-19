4ec437967b7c6201e39d130d794d311db08fb35e
xadogix539 <xadogix539@gmail.com>
2024-10-19 14:34:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "jNcFiTwZ#vUlZyyhB92QiI_1X4_xOL5GlP6pATs_RhqH7KYK_AiE",
+SESSION_ID: process.env.SESSION_ID || "8bdzQaiT#-xPPANPLSw-VElMfgBr8n11VUbDUW9QR8SaynFTDGJg",
 MONGODB: process.env.MONGODB || "mongodb://mongo:ugUHcTyXvgPmrGpmWtrCwrrWoUSQxDbZ@autorack.proxy.rlwy.net:11616",
 PREFIX: process.env.PREFIX || ".",
 BOT_NAME: process.env.BOT_NAME || "Thenu-MD",
-BOT_NUMBER: process.env.BOT_NUMBER || "94767096711",
+BOT_NUMBER: process.env.BOT_NUMBER || "919023369703",
 AUTO_AI: process.env.AUTO_AI || "false",
 LANG: process.env.LANG || "SI"
 
